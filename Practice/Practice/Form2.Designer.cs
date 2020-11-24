704aec8f1affc2ab5af5d62855b2f7c8a88229f1
FleksKavskii <66283133+FleksKavskii@users.noreply.github.com>
2020-11-24 20:46:37 +0300

    KAA_Task_PersonalProject

---
+++ Practice/Practice/Form2.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace Practice
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-4, -3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(651, 410);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(14, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Выберите число";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12"});
+            this.comboBox1.Location = new System.Drawing.Point(19, 123);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(170, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(19, 295);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(170, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(14, 249);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(181, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Сделайте ставку";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(211, 338);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 36);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Сделать ставку";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(430, 56);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(93, 116);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(391, 222);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 25);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Ваш баланс:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(564, 222);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 25);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "100";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(537, 56);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(93, 116);
+            this.textBox3.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(14, 167);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(410, 25);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Выбранное число больше или меньше?";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton1.Location = new System.Drawing.Point(32, 207);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(39, 28);
+            this.radioButton1.TabIndex = 13;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = ">";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton2.Location = new System.Drawing.Point(117, 207);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(39, 28);
+            this.radioButton2.TabIndex = 14;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "<";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(438, 28);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(70, 25);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(542, 28);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(70, 25);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "label8";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 404);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
---
+++ Practice/Practice/Form2.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practice
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        int balance = 100;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int bet = 0;
+            int numbers;
+            numbers = comboBox1.SelectedIndex + 2;
+            int firstDice = rnd.Next(1, 6);
+            int secondDice = rnd.Next(1, 6);
+            label7.Text = Convert.ToString(firstDice);
+            label8.Text = Convert.ToString(secondDice);
+            bet = Convert.ToInt32(textBox1.Text);
+            if (radioButton2.Checked)
+            {
+                if (numbers < (firstDice + secondDice))
+                    balance -= bet;
+                else if (numbers > (firstDice + secondDice))
+                    balance += bet;
+                else if (numbers == (firstDice + secondDice))
+                    balance += (bet * 4);
+            }
+            else if (radioButton1.Checked)
+            {
+                if (numbers > (firstDice + secondDice))
+                    balance -= bet;
+                else if (numbers < (firstDice + secondDice))
+                    balance += bet;
+                else if (numbers == (firstDice + secondDice))
+                    balance += (bet * 4);
+            }
+            if (firstDice == 6)
+            {
+                string[,] FirstDice =
+                      {
+                    {" #","#","#","#","#","#"},
+                    {" # ","","o","o","o"," #"},
+                    {" # ","","o","o","o"," #"},
+                    {" #","#","#","#","#","#"},
+                   };
+                for (int i = 0; i < FirstDice.GetLength(0); i++)
+                {
+                    textBox2.AppendText("\r");
+                    textBox2.AppendText("\n");
+                    for (int j = 0; j < FirstDice.GetLength(1); j++)
+                    {
+                        textBox2.AppendText("");
+                        textBox2.AppendText(FirstDice[i, j]);
+
+                    }
+                }
+            }
+            if (firstDice == 1)
+            {
+                string[,] FirstDice =
+                      {
+                    {" #","#","#","#","#","#"},
+                    {" # "," "," ","o","  "," #"},
+                    {" # "," "," "," ","   "," #"},
+                    {" #","#","#","#","#","#"},
+                   };
+                for (int i = 0; i < FirstDice.GetLength(0); i++)
+                {
+                    textBox2.AppendText("\r");
+                    textBox2.AppendText("\n");
+                    for (int j = 0; j < FirstDice.GetLength(1); j++)
+                    {
+                        textBox2.AppendText("");
+                        textBox2.AppendText(FirstDice[i, j]);
+
+                    }
+                }
+            }
+            if (firstDice == 3)
+            {
+                string[,] FirstDice =
+                      {
+                    {" #","#","#","#","#","#"},
+                    {" # ","o","o","o",""," #"},
+                    {" # "," "," "," ","   "," #"},
+                    {" #","#","#","#","#","#"},
+                   };
+                for (int i = 0; i < FirstDice.GetLength(0); i++)
+                {
+                    textBox2.AppendText("\r");
+                    textBox2.AppendText("\n");
+                    for (int j = 0; j < FirstDice.GetLength(1); j++)
+                    {
+                        textBox2.AppendText("");
+                        textBox2.AppendText(FirstDice[i, j]);
+
+                    }
+                }
+            }
+            if (firstDice == 2)
+            {
+                string[,] FirstDice =
+                      {
+                    {" #","#","#","#","#","#"},
+                    {" # "," ","o","o"," "," #"},
+                    {" # "," "," "," ","   "," #"},
+                    {" #","#","#","#","#","#"},
+                   };
+                for (int i = 0; i < FirstDice.GetLength(0); i++)
+                {
+                    textBox2.AppendText("\r");
+                    textBox2.AppendText("\n");
+                    for (int j = 0; j < FirstDice.GetLength(1); j++)
+                    {
+                        textBox2.AppendText("");
+                        textBox2.AppendText(FirstDice[i, j]);
+
+                    }
+                }
+            }
+            if (firstDice == 4)
+            {
+                string[,] FirstDice =
+                      {
+                    {" #","#","#","#","#","#"},
+                    {" # ","","","o"," o","  #"},
+                    {" # ","","","o"," o","  #"},
+                    {" #","#","#","#","#","#"},
+                   };
+                for (int i = 0; i < FirstDice.GetLength(0); i++)
+                {
+                    textBox2.AppendText("\r");
+                    textBox2.AppendText("\n");
+                    for (int j = 0; j < FirstDice.GetLength(1); j++)
+                    {
+                        textBox2.AppendText("");
+                        textBox2.AppendText(FirstDice[i, j]);
+
+                    }
+                }
+            }
+            if (firstDice == 5)
+            {
+                string[,] FirstDice =
+                      {
+                    {" #","#","#","#","#","#"},
+                    {" # ","","o","o","o"," #"},
+                    {" # ","",""," o","o","  #"},
+                    {" #","#","#","#","#","#"},
+                   };
+                for (int i = 0; i < FirstDice.GetLength(0); i++)
+                {
+                    textBox2.AppendText("\r");
+                    textBox2.AppendText("\n");
+                    for (int j = 0; j < FirstDice.GetLength(1); j++)
+                    {
+                        textBox2.AppendText("");
+                        textBox2.AppendText(FirstDice[i, j]);
+
+                    }
+                }
+            }
+            if (secondDice == 6)
+            {
+                string[,] SecondDice =
+                      {
+                    {" #","#","#","#","#","#"},
+                    {" # ","","o","o","o"," #"},
+                    {" # ","","o","o","o"," #"},
+                    {" #","#","#","#","#","#"},
+                   };
+                for (int i = 0; i < SecondDice.GetLength(0); i++)
+                {
+                    textBox3.AppendText("\r");
+                    textBox3.AppendText("\n");
+                    for (int j = 0; j < SecondDice.GetLength(1); j++)
+                    {
+                        textBox3.AppendText("");
+                        textBox3.AppendText(SecondDice[i, j]);
+
+                    }
+                }
+            }
+            if (secondDice == 1)
+            {
+                string[,] SecondDice =
+                      {
+                    {" #","#","#","#","#","#"},
+                    {" # "," "," ","o","  "," #"},
+                    {" # "," "," "," ","   "," #"},
+                    {" #","#","#","#","#","#"},
+                   };
+                for (int i = 0; i < SecondDice.GetLength(0); i++)
+                {
+                    textBox3.AppendText("\r");
+                    textBox3.AppendText("\n");
+                    for (int j = 0; j < SecondDice.GetLength(1); j++)
+                    {
+                        textBox3.AppendText("");
+                        textBox3.AppendText(SecondDice[i, j]);
+
+                    }
+                }
+            }
+            if (secondDice == 3)
+            {
+                string[,] SecondDice =
+                      {
+                    {" #","#","#","#","#","#"},
+                    {" # ","o","o","o",""," #"},
+                    {" # "," "," "," ","   "," #"},
+                    {" #","#","#","#","#","#"},
+                   };
+                for (int i = 0; i < SecondDice.GetLength(0); i++)
+                {
+                    textBox3.AppendText("\r");
+                    textBox3.AppendText("\n");
+                    for (int j = 0; j < SecondDice.GetLength(1); j++)
+                    {
+                        textBox3.AppendText(SecondDice[i, j]);
+
+                    }
+                }
+            }
+            if (secondDice == 2)
+            {
+                string[,] SecondDice =
+                      {
+                    {" #","#","#","#","#","#"},
+                    {" # "," ","o","o"," "," #"},
+                    {" # "," "," "," ","   "," #"},
+                    {" #","#","#","#","#","#"},
+                   };
+                for (int i = 0; i < SecondDice.GetLength(0); i++)
+                {
+                    textBox3.AppendText("\r");
+                    textBox3.AppendText("\n");
+                    for (int j = 0; j < SecondDice.GetLength(1); j++)
+                    {
+                        textBox3.AppendText("");
+                        textBox3.AppendText(SecondDice[i, j]);
+
+                    }
+                }
+            }
+            if (secondDice == 4)
+            {
+                string[,] SecondDice =
+                      {
+                    {" #","#","#","#","#","#"},
+                    {" # ","","","o"," o","  #"},
+                    {" # ","","","o"," o","  #"},
+                    {" #","#","#","#","#","#"},
+                   };
+                for (int i = 0; i < SecondDice.GetLength(0); i++)
+                {
+                    textBox3.AppendText("\r");
+                    textBox3.AppendText("\n");
+                    for (int j = 0; j < SecondDice.GetLength(1); j++)
+                    {
+                        textBox3.AppendText("");
+                        textBox3.AppendText(SecondDice[i, j]);
+
+                    }
+                }
+            }
+            if (secondDice == 5)
+            {
+                string[,] SecondDice =
+                      {
+                    {" #","#","#","#","#","#"},
+                    {" # ","","o","o","o"," #"},
+                    {" # ","",""," o","o","  #"},
+                    {" #","#","#","#","#","#"},
+                   };
+                for (int i = 0; i < SecondDice.GetLength(0); i++)
+                {
+                    textBox3.AppendText("\r");
+                    textBox3.AppendText("\n");
+                    for (int j = 0; j < SecondDice.GetLength(1); j++)
+                    {
+                        textBox3.AppendText("");
+                        textBox3.AppendText(SecondDice[i, j]);
+
+                    }
+                }
+            }
+            label4.Text = Convert.ToString(balance);
+        }
+    }
+}
